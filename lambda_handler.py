712359fa0fbe7ab0a215bbc25319b6607906ab34
Canyon Smith <canyon.smith@octanner.com>
2023-12-25 14:50:53 -0700

    added tests

--- lambda_handler.py
+++ lambda_handler.py
@@ -2,72 +2,68 @@ import boto3
 import base64
 import uuid
 import os
+import logging
+from botocore.exceptions import BotoCoreError, ClientError
 
-def process_image(image_binary_data):
-    # Initialize Textract client
-    textract = boto3.client('textract', region_name='us-west-2')
+class ImageProcessor:
+    def __init__(self, s3_bucket, aws_region):
+        self.s3 = boto3.client('s3', region_name=aws_region)
+        self.textract = boto3.client('textract', region_name=aws_region)
+        self.bucket = s3_bucket
+        self.logger = logging.getLogger(__name__)
 
-    # Call Textract to extract text
-    response = textract.detect_document_text(Document={'Bytes': image_binary_data})
-
-    # Process and return the response
-    text = ""
-    for item in response['Blocks']:
-        if item['BlockType'] == 'LINE':
-            text += item['Text'] + '\n'
-    print(text,'text')
-    return text
-
-def lambda_handler(event, context):
-    try:
-        # Initialize S3 client
-        s3 = boto3.client('s3', region_name='us-west-2')
-
-        # Define S3 bucket
-        bucket = 'publicbucketszoomrecord'
-
-        # Get base64 image from the POST request body
-        base64_image = event['body']
-
-        # Decode base64 image
+    def decode_image(self, base64_image):
         try:
-            image_binary_data = base64.b64decode(base64_image)
+            return base64.b64decode(base64_image)
         except Exception as e:
-            print(f"Error decoding base64 image: {e}")
-            return {'statusCode': 500, 'body': f"Error decoding base64 image: {e}"}
+            self.logger.error(f"Error decoding base64 image: {e}")
+            raise ValueError("Invalid base64 image") from e
 
-        # Generate a unique file name
+    def save_image(self, image_binary_data):
         file_name = os.path.join('/tmp', str(uuid.uuid4()) + '.png')
+        try:
+            with open(file_name, 'wb') as file:
+                file.write(image_binary_data)
+        except Exception as e:
+            self.logger.error(f"Error saving image: {e}")
+            raise IOError("Error saving image to disk") from e
+        return file_name
 
-        # Write the image to a file in the /tmp directory
-        with open(file_name, 'wb') as file:
-            file.write(image_binary_data)
-
-        print(f"Image saved as {file_name}.")
-
-        # Upload the image to S3
+    def upload_image_to_s3(self, file_name):
         try:
             with open(file_name, 'rb') as file:
-                s3.put_object(Body=file, Bucket=bucket, Key=os.path.basename(file_name), ContentType='image/png')
-        except Exception as e:
-            print(f"Error uploading image to S3: {e}")
-            return {'statusCode': 500, 'body': f"Error uploading image to S3: {e}"}
+                self.s3.put_object(Body=file, Bucket=self.bucket, Key=os.path.basename(file_name), ContentType='image/png')
+        except (BotoCoreError, ClientError) as e:
+            self.logger.error(f"Error uploading image to S3: {e}")
+            raise Exception("Error uploading image to S3") from e
 
-        # Process the image with Textract
+    def process_image(self, image_binary_data):
         try:
-            extracted_text = process_image(image_binary_data)
-        except Exception as e:
-            print(f"Error processing image with Textract: {e}")
-            return {'statusCode': 500, 'body': f"Error processing image with Textract: {e}"}
+            response = self.textract.detect_document_text(Document={'Bytes': image_binary_data})
+            return '\n'.join(item['Text'] for item in response['Blocks'] if item['BlockType'] == 'LINE')
+        except (BotoCoreError, ClientError) as e:
+            self.logger.error(f"Error processing image with Textract: {e}")
+            raise Exception("Error processing image with Textract") from e
 
-        # Optionally, clean up the image from /tmp if you're concerned about storage
+    def cleanup(self, file_name):
         try:
             os.remove(file_name)
         except Exception as e:
-            print(f"Warning: Could not remove temporary file {file_name}: {e}")
+            self.logger.error(f"Error during cleanup: {e}")
+            raise IOError("Error during cleanup") from e
 
+def lambda_handler(event, context):
+    aws_region = os.getenv('AWS_REGION', 'us-west-2')
+    s3_bucket = os.getenv('S3_BUCKET_NAME', 'publicbucketszoomrecord')
+    processor = ImageProcessor(s3_bucket, aws_region)
+
+    try:
+        base64_image = event['body']
+        image_binary_data = processor.decode_image(base64_image)
+        file_name = processor.save_image(image_binary_data)
+        processor.upload_image_to_s3(file_name)
+        extracted_text = processor.process_image(image_binary_data)
+        processor.cleanup(file_name)
         return {'statusCode': 200, 'body': extracted_text}
-
     except Exception as e:
-        print(e)
         return {'statusCode': 500, 'body': str(e)}
---
+++ test_lambda.py
@@ -0,0 +1,38 @@
+import requests
+from PIL import Image
+from io import BytesIO
+import base64
+from lambda_handler import lambda_handler  # assuming your main file is named image_processor.py
+
+def test_lambda_handler():
+    # Define the image URL
+    image_url = "https://cdn1.sportngin.com/attachments/photo/8993/1181/come-on-lets-party_large.jpg"
+
+    # Download the image
+    response = requests.get(image_url)
+
+    # Open the image with PIL
+    image = Image.open(BytesIO(response.content))
+
+    # Convert the image to base64
+    base64_image = base64.b64encode(response.content).decode('utf-8')
+
+    # Define a test event
+    event = {
+        'body': base64_image
+    }
+
+    # Call the lambda handler
+    result = lambda_handler(event, None)
+
+    # Extract the text from the body response
+    text = result['body']
+
+    # Lower case the text
+    text = text.lower()
+
+    # Check if 'party' is in the text
+    assert 'party' in text, "Test failed: 'party' not found in the response text"
+
+if __name__ == "__main__":
+    print(test_lambda_handler())
